4a8ca15b92f39a58d8d3dc0c2d29e40fcd3c859e
Nathan Conde <82476114+NathanConde-dev@users.noreply.github.com>
2024-06-01 22:04:34 -0300

    Ajuste de CORS

--- src/server.ts
+++ src/server.ts
@@ -5,26 +5,29 @@ import { setupSwagger } from './config/swagger';
 import routes from './app/routes/routes';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import bodyParser from 'body-parser';
+
 dotenv.config();
 
 const app: Express = express();
 
 // Permitir todas as origens
 app.use(cors());
-app.use(express.json());
 
-//Definição da porta pelo Railway
+// Middleware para parsing do body da requisição
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Definição da porta pelo Railway
 const PORT = process.env.PORT || 3010;
 
 AppDataSource.initialize().then(() => {
-  
     console.log("Data Source has been initialized and cache cleared!");
     
     setupSwagger(app);
     app.use('/', routes);
     
-    app.listen(PORT, () => console.log('Server started'));
+    app.listen(PORT, () => console.log('Server started on port', PORT));
 }).catch((error: Error) => {
-
     console.error('Error during Data Source initialization:', error);
 });
